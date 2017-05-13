1769d2225f384935d419a0288e69d5160286d172
Rafał Cieślak <ravicious@gmail.com>
2017-05-13 14:27:20 +0200

    Render a full-sized table of square cells

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,28 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+cells =
+    List.range 1 (16 * 30)
+        |> List.map
+            (\n ->
+                let
+                    power =
+                        n % 6
+                in
+                    div [ class "grid-cell" ]
+                        [ if power == 0 then
+                            text
+                                ""
+                          else
+                            text (toString power)
+                        ]
+            )
 
 
 main : Html.Html msg
 main =
-    text "Hello, World!"
+    div [ class "grid grid--16x30" ]
+        cells
